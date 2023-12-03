60f6a555b4980a573dc9cc6d316bde54a21b72b0
mohasinakonda <mohasinakonda39@gmail.com>
2023-12-03 22:08:44 +0600

    chore: create migrations and make relationship

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'excerpt', 'description', 'author'];
+    public function reviews()
+    {
+        $this->hasMany('review');
+    }
+    public function user()
+    {
+        $this->hasMany('user');
+    }
 }
---
+++ app/Models/Review.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+    protected $fillable = ['review', 'rating'];
+    public function book()
+    {
+        $this->belongsTo('book');
+    }
+}
