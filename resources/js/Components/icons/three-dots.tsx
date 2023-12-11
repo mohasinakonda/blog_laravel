4cf9f8fd61dfbfdcecca321240129985c3d7b11f
mohasinakonda <mohasinakonda39@gmail.com>
2023-12-11 22:20:36 +0600

    chore: add icons for action buttons

---
+++ resources/js/Components/icons/three-dots.tsx
@@ -0,0 +1,53 @@
+export const ThreeDots = () => (
+    <svg
+        width="24px"
+        height="24px"
+        viewBox="0 0 24 24"
+        id="three-dots"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#000000"
+    >
+        <g id="SVGRepo_iconCarrier">
+            {" "}
+            <g
+                id="_20x20_three-dots--grey"
+                data-name="20x20/three-dots--grey"
+                transform="translate(24) rotate(90)"
+            >
+                <rect id="Rectangle" width="24" height="24" fill="none"></rect>{" "}
+                <circle
+                    id="Oval"
+                    cx="1"
+                    cy="1"
+                    r="1"
+                    transform="translate(5 11)"
+                    stroke="#000000"
+                    strokeMiterlimit="10"
+                    strokeWidth="1"
+                ></circle>{" "}
+                <circle
+                    id="Oval-2"
+                    data-name="Oval"
+                    cx="1"
+                    cy="1"
+                    r="1"
+                    transform="translate(11 11)"
+                    stroke="#000000"
+                    strokeMiterlimit="10"
+                    strokeWidth="1"
+                ></circle>{" "}
+                <circle
+                    id="Oval-3"
+                    data-name="Oval"
+                    cx="1"
+                    cy="1"
+                    r="1"
+                    transform="translate(17 11)"
+                    stroke="#000000"
+                    strokeMiterlimit="10"
+                    strokeWidth="1"
+                ></circle>{" "}
+            </g>{" "}
+        </g>
+    </svg>
+);
